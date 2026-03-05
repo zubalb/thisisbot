9cba3275e4a5532cab8f8ba1457ec95687870356
zubalb <pendlumlive@gmail.com>
2026-03-05 20:50:22 +0300

    Adding async bot

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+import asyncio
+import os
+
+from telebot.async_telebot import AsyncTeleBot
+
+bot = AsyncTeleBot(os.environ["tgtoken"])
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    text = 'Hi, I am EchoBot.\nJust write me something and I will repeat it!'
+    await bot.reply_to(message, text)
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
+
+if __name__ == '__main__':
+    asyncio.run(bot.polling())
